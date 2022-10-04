895279c80ecc892f4233aa022110d7993b5d0236
Pankajkumar90 <pankajbaliyan90@gmail.com>
2022-10-04 21:50:59 +0530

    improve code & fix bugs on JS file

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -3,6 +3,7 @@ let startonTooltip = document.getElementById("startonTooltip"); // on btn toolti
 let startButton = document.getElementById("starton"); //on btn txt
 let outputResult = document.getElementById("outputResult"); //result data show here
 let result = document.getElementById("result"); //last result data show here
+let resultStatus = 0;
 
 //function to on / off calculator
 function startCalc() {
@@ -19,14 +20,14 @@ function startCalc() {
     }
 }
 
-// let previousResult =  document.getElementById("outputResult").value;
-
 function notification(msgHere, notificationStatus, notificationIcon, inputNumber) {
     if (startButton.innerHTML == "ON") {
         notificationHere('Turn On your Calc', 'red', 'question');
     } else {
-        // console.log(previousResult)
         notificationHere(msgHere, notificationStatus, notificationIcon);
+        if(resultStatus == 1) {
+            outputResult.value = 0;
+        }
         enterNumber(inputNumber)
     }
 }
@@ -49,24 +50,38 @@ function enterNumber(inputNumber) {
     let finalData = currentData.concat(addData);
 
     if (previousData == 0) {
-        outputResult.value = inputNumber;
+        if(inputNumber == 'backSpace') {
+            outputResult.value = 0;
+        } else if(inputNumber == 'clear') {
+            outputResult.value = 0;
+        }else {
+            outputResult.value = inputNumber;
+        }
+        resultStatus = 0;
     } else if (inputNumber == 'clear') { // used to clear the screen
-        document.getElementById("outputResult").value = 0;
-    } else if (inputNumber == '=') {
+        outputResult.value = 0;
+        resultStatus = 0;
+    } else if(inputNumber == 'backSpace') {
+        let str = finalData.slice(0, -10);
+        outputResult.value = str;
+        if(outputResult.value == '' || outputResult.value == ' ' || outputResult.value == 'backSpace') {
+            outputResult.value = 0;
+        }
+    }else if (inputNumber == '=') {
         calculation(finalData);
     } else {
+        resultStatus = 0;
         outputResult.value = finalData;
     }
 }
 
 //make calculation function
 function calculation(inputData) {
-    console.log(inputData)
-    console.log(inputData.charAt(inputData.length - 1))
     let str = inputData.slice(0, -1); //remove last digit(=) from outputResult.value
     let setData = eval(str); //perform javascript operations on string values
     outputResult.value = setData;
     result.value = setData;
+    resultStatus = 1;
 }
 
 
