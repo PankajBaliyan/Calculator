ae8120398e6fe80164c6811f6adebbebcb5aa98e
Pankajkumar90 <pankajbaliyan90@gmail.com>
2022-10-05 10:30:05 +0530

    improve buttons functionalities

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -4,6 +4,7 @@ let startButton = document.getElementById("starton"); //on btn txt
 let outputResult = document.getElementById("outputResult"); //result data show here
 let result = document.getElementById("result"); //last result data show here
 let resultStatus = 0;
+let calculatorStatus = 0;
 
 //function to on / off calculator
 function startCalc() {
@@ -11,24 +12,75 @@ function startCalc() {
         startButton.innerHTML = "OFF"; //change btn txt
         startonTooltip.innerHTML = "Off"; //change btn tooltip
         outputResult.value = 0; //set value in result
-        notificationHere('Now Calculator is ON', 'success');
+        notificationHere("Now Calculator is ON", "success");
+        calculatorStatus = 1;
     } else {
         startButton.innerHTML = "ON"; //change btn txt
         startonTooltip.innerHTML = "On"; //change btn tooltip
         outputResult.value = ""; //remove value in result
-        notificationHere('Now Calculator is OFF', 'danger');
+        notificationHere("Now Calculator is OFF", "danger");
+        calculatorStatus = 0;
     }
 }
 
-function notification(msgHere, notificationStatus, notificationIcon, inputNumber) {
+// for disable click behavior on input field before the calculator is on
+outputResult.addEventListener("click", function () {
+    if (calculatorStatus == 1) {
+        outputResult.focus();
+    } else {
+        outputResult.blur();
+        notificationHere("Turn On your Calc", "red", "question");
+    }
+});
+
+// set custom input for input field
+var onlyNumberKey = function (ev) {
+    var key;
+    var isShift;
+    if (window.event) {
+        key = window.event.keyCode;
+        isShift = !!window.event.shiftKey; // typecast to boolean
+    } else {
+        key = ev.which;
+        isShift = !!ev.shiftKey;
+    }
+    if (isShift) {
+        switch (key) {
+            case 43:
+                return true;
+                break;
+            case 42:
+                return true;
+                break;
+            case 37:
+                return true;
+                break;
+            default:
+                return false;
+                break;
+        }
+    } else if(key == 45 || key == 46 || key == 47 || key >= 48 && key <= 57) {
+        return true;
+    } else {
+        return false;
+    }
+};
+// end here
+
+function notification(
+    msgHere,
+    notificationStatus,
+    notificationIcon,
+    inputNumber
+) {
     if (startButton.innerHTML == "ON") {
-        notificationHere('Turn On your Calc', 'red', 'question');
+        notificationHere("Turn On your Calc", "red", "question");
     } else {
         notificationHere(msgHere, notificationStatus, notificationIcon);
-        if(resultStatus == 1) {
+        if (resultStatus == 1) {
             outputResult.value = 0;
         }
-        enterNumber(inputNumber)
+        enterNumber(inputNumber);
     }
 }
 
@@ -50,24 +102,29 @@ function enterNumber(inputNumber) {
     let finalData = currentData.concat(addData);
 
     if (previousData == 0) {
-        if(inputNumber == 'backSpace') {
+        if (inputNumber == "backSpace") {
             outputResult.value = 0;
-        } else if(inputNumber == 'clear') {
+        } else if (inputNumber == "clear") {
             outputResult.value = 0;
-        }else {
+        } else {
             outputResult.value = inputNumber;
         }
         resultStatus = 0;
-    } else if (inputNumber == 'clear') { // used to clear the screen
+    } else if (inputNumber == "clear") {
+        // used to clear the screen
         outputResult.value = 0;
         resultStatus = 0;
-    } else if(inputNumber == 'backSpace') {
+    } else if (inputNumber == "backSpace") {
         let str = finalData.slice(0, -10);
         outputResult.value = str;
-        if(outputResult.value == '' || outputResult.value == ' ' || outputResult.value == 'backSpace') {
+        if (
+            outputResult.value == "" ||
+            outputResult.value == " " ||
+            outputResult.value == "backSpace"
+        ) {
             outputResult.value = 0;
         }
-    }else if (inputNumber == '=') {
+    } else if (inputNumber == "=") {
         calculation(finalData);
     } else {
         resultStatus = 0;
@@ -83,5 +140,3 @@ function calculation(inputData) {
     result.value = setData;
     resultStatus = 1;
 }
-
-
