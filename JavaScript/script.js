f7184d3176eb19769fa6e63960cdd08568a39494
Pankajkumar90 <pankajbaliyan90@gmail.com>
2023-03-04 02:07:09 +0530

    improve calculation power

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,275 +1,234 @@
 //declare variables
-let startonTooltip = document.getElementById("startonTooltip"); // on btn tooltip txt
-let startButton = document.getElementById("starton"); //on btn txt
-let outputResult = document.getElementById("outputResult"); //result data show here
+const changeImageButton = document.querySelector("#change-image");
+const backSpaceImage = document.querySelector("#backSpacePNG");
+const startCalcButton = document.querySelector("#start-calc");
+const startButton = document.querySelector("#starton");
+const startonTooltip = document.querySelector("#startonTooltip");
+const outputResult = document.querySelector("#outputResult");
+
 let result = document.getElementById("result"); //last result data show here
-let backSpacePNG = document.getElementById("backSpacePNG"); //backspace img
 let resultStatus = 0;
 let calculatorStatus = 0;
 let notificationPosition;
 
-
+// function to show notification on file loads
 window.onload = () => {
-    notificationHere("Welcome to Calculator", "success", "check");
-}
-
-// function to change backspace image on mouseenter
-function changeImage() {
-    backSpacePNG.src = "images/backspaceWhite.png";
-}
-
-// function to change backspace image on mouseleave
-function revertImage() {
-    backSpacePNG.src = "images/backspace.png";
-}
-
-//function to on / off calculator
-function startCalc() {
-    if (startButton.innerHTML == "ON") {
-        startButton.innerHTML = "OFF";
-        startonTooltip.innerHTML = "Off";
-        outputResult.value = 0; //set value in result
-        notificationHere("Now Calculator is ON", "success");
-        calculatorStatus = 1;
-    } else {
-        startButton.innerHTML = "ON";
-        startonTooltip.innerHTML = "On";
-        outputResult.value = ""; //remove value in result
-        notificationHere("Now Calculator is OFF", "danger");
-        calculatorStatus = 0;
-    }
-}
-
-// get notification according to keypress
-onkeydown = function (e) {
-    if (calculatorStatus == 1) {
-        // Make Calculator perform operation on keypress
-        switch (e.keyCode) {
-            case 97:
-                notification('One', 'None', '#', 1);
-                break;
-            case 98:
-                notification('Two', 'None', '#', 2);
-                break;
-            case 99:
-                notification('Three', 'None', '#', 3);
-                break;
-            case 100:
-                notification('Four', 'None', '#', 4);
-                break;
-            case 101:
-                notification('Five', 'None', '#', 5);
-                break;
-            case 102:
-                notification('Six', 'None', '#', 6);
-                break;
-            case 103:
-                notification('Seven', 'None', '#', 7);
-                break;
-            case 104:
-                notification('Eight', 'None', '#', 8);
-                break;
-            case 105:
-                notification('Nine', 'None', '#', 9);
-                break;
-            case 96:
-                notification('Zero', 'None', '#', 0);
-                break;
-            case 107:
-                notification('Plus', 'None', '#', '+');
-                break;
-            case 109:
-                notification('Minus', 'None', '#', '-');
-                break;
-            case 106:
-                notification('Multiply', 'None', '#', '*');
-                break;
-            case 111:
-                notification('Divide', 'None', '#', '/');
-                break;
-            case 110:
-                notification('Dot', 'None', '#', '.');
-                break;
-            case 189:
-                notification('Minus', 'None', '#', '-');
-                break;
-            case 187:
-                notification('=', 'warning', '#', '=')
-                break;
-            case 13:
-                notification('=', 'warning', '#', '=')
-                break;
-            case 8:
-                notification('Backspace', 'warning', 'B', 'backSpace')
-                break;
-            case 46:
-                notification('Clear', 'info', 'X', 'clear')
-                break;
-            default:
-                notificationHere("Invalid Key Pressed", "red", "question");
-                break;
-        }
-
-        let isShift;
-        if (window.event) {
-            isShift = !!window.event.shiftKey; // typecast to boolean
-        } else {
-            isShift = !!ev.shiftKey;
-        }
-        if (isShift) {
-            if (e.keyCode == 53) {
-                notification('Percentage', 'None', '#', '%');
-            } else if (e.keyCode == 56) {
-                notification('Multiply', 'None', '#', '*');
-            } else if (e.keyCode == 187) {
-                notification('Plus', 'None', '#', '+');
-            } else {
-                notificationHere("Invalid Key Pressed", "red", "question");
-            }
-        }
-
-    } else {
-        notificationHere("Turn On your Calc", "red", "question");
-    }
+    showNotification("Welcome to Calculator", "success", "check");
 };
 
-// function to set data in notification function
-function notification(
-    msgHere,
-    notificationStatus,
-    notificationIcon,
-    inputNumber
-) {
-    if (startButton.innerHTML == "ON") {
-        notificationHere("Turn On your Calc", "red", "question");
-    } else {
-        notificationHere(msgHere, notificationStatus, notificationIcon);
-        if (resultStatus == 1) {
-            outputResult.value = 0;
-        }
+//To change backSpace button on calculator START
+changeImageButton.addEventListener("mouseenter", onMouseEnter);
+changeImageButton.addEventListener("mouseleave", onMouseLeave);
+function onMouseEnter() {
+    backSpaceImage.src = `images/backspaceWhite.png`;
+}
 
-        let previousData = outputResult.value;
-        let currentData = previousData.toString();
-        if (inputNumber == "+" || inputNumber == "-" || inputNumber == "*" || inputNumber == "/" || inputNumber == "%") {
-            let lastDigit = currentData.charAt(currentData.length - 1);
-            if (lastDigit == "+" || lastDigit == "-" || lastDigit == "*" || lastDigit == "/" || lastDigit == "%") {
-                currentData = currentData.slice(0, -1); // remove last digit if it is symbol, to resolve the conflict of multiple symbols
-            }
-        }
-        enterNumber(inputNumber, currentData);
+function onMouseLeave() {
+    backSpaceImage.src = `images/backspace.png`;
+}
+
+// Function to start/stop calculator
+startCalcButton.addEventListener("click", startCalc);
+document.addEventListener("keydown", (event) => {
+    if (event.keyCode === 79) {
+        startCalc();
     }
+});
+
+
+function startCalc() {
+    const isOn = startButton.innerHTML === "ON";
+    startButton.innerHTML = isOn ? "OFF" : "ON";
+    startonTooltip.innerHTML = isOn ? "Off" : "On";
+    outputResult.value = isOn ? 0 : "";
+    showNotification(`Now Calculator is ${isOn ? "ON" : "OFF"}`, isOn ? "success" : "danger");
+    calculatorStatus = isOn ? 1 : 0;
+}
+
+// function to set data in notification function
+function notification(msgHere, notificationStatus, notificationIcon, inputNumber) {
+    if (startButton.innerHTML === "ON") {
+        showNotification("Turn On your Calc", "red", "question");
+        return;
+    }
+
+    showNotification(msgHere, notificationStatus, notificationIcon);
+
+    if (resultStatus === 1) {
+        outputResult.value = 0;
+    }
+
+    const currentData = outputResult.value.toString();
+
+    if (["+", "-", "*", "/", "%"].includes(inputNumber)) {
+        const lastDigit = currentData.charAt(currentData.length - 1);
+
+        if (["+", "-", "*", "/", "%"].includes(lastDigit)) {
+            outputResult.value = currentData.slice(0, -1);
+        }
+    }
+
+    enterNumber(inputNumber, outputResult.value);
 }
 
 // function to set notification position according to screen size
-function notificationPos() {
-    let windowInnerWidth = window.innerWidth;
-    if (windowInnerWidth < 954) {
-        notificationPosition = "tr";
-    } else {
-        notificationPosition = "br";
-    }
+function setNotificationPosition() {
+    const windowInnerWidth = window.innerWidth;
+    notificationPosition = windowInnerWidth < 954 ? "tr" : "br";
 }
 
 // toast Notification here
-function notificationHere(msgHere, notificationStatus, notificationIcon) {
-    notificationPos()
+function showNotification(msg, status, icon) {
+    setNotificationPosition();
     SnackBar({
         timeout: 1000, // ms
         position: notificationPosition,
-        message: msgHere,
+        message: msg,
         speed: "0.2s",
-        status: notificationStatus,
-        icon: notificationIcon,
+        status: status,
+        icon: icon,
     });
 }
 
-// function that manage data inside inputBox
+// function that manage data inside inputBox of calculator & based on key pressed & input key
 function enterNumber(inputNumber, currentData) {
     let previousData = outputResult.value;
-    if (currentData == undefined || currentData == null || currentData == "") {
+    if (!currentData) {
         currentData = previousData.toString();
     }
-    let addData = inputNumber.toString();
-    let finalData = currentData.concat(addData);
-
+    let finalData = currentData + inputNumber;
+    let lastDigit = currentData.charAt(currentData.length - 1);
     if (previousData == 0) {
-        if (inputNumber == "backSpace" || inputNumber == "=") {
-            outputResult.value = 0;
-        } else if (inputNumber == "clear") {
+        if (inputNumber == "backSpace" || inputNumber == "=" || inputNumber == "clear") {
             outputResult.value = 0;
         } else {
             outputResult.value = inputNumber;
         }
         resultStatus = 0;
-    } else if (inputNumber == "clear") {
+    } else if (inputNumber === "clear") {
         outputResult.value = 0;
         resultStatus = 0;
-    } else if (inputNumber == "backSpace") {
-        let str = finalData.slice(0, -10);
-        outputResult.value = str;
-        if (
-            outputResult.value == "" ||
-            outputResult.value == " " ||
-            outputResult.value == "backSpace"
-        ) {
+    } else if (inputNumber === "backSpace") {
+        outputResult.value = finalData.slice(0, -1);
+        if (!outputResult.value || outputResult.value === "backSpace") {
             outputResult.value = 0;
         }
-    } else if (inputNumber == "=") {
-        if (inputNumber != currentData.charAt(currentData.length - 1)) {
-            calculation(finalData);
-        }
-    } else if (inputNumber == "+" || inputNumber == "-" || inputNumber == "*" || inputNumber == "/" || inputNumber == "%" || inputNumber == ".") {
-        if (inputNumber != currentData.charAt(currentData.length - 1)) {
-            outputResult.value = finalData;
-        }
+    } else if (inputNumber === "=" && inputNumber !== lastDigit) {
+        calculation(finalData);
+    } else if ((/[+\-*/%.]/).test(inputNumber) && inputNumber !== lastDigit) {
+        outputResult.value = finalData;
     } else {
         resultStatus = 0;
         outputResult.value = finalData;
     }
 }
 
-//make calculation function
+// function to make calculations and display result
 function calculation(inputData) {
     let currentData = outputResult.value.toString();
     let str = inputData.slice(0, -1); //remove last digit(=) from outputResult.value
 
-    // checking for last digit is symbol or not, until all symbols are not removed this loop will run
-    let checkingSymbolAtLast = str.charAt(str.length - 1);
-    while (checkingSymbolAtLast == '+' || checkingSymbolAtLast == '-' || checkingSymbolAtLast == '*' || checkingSymbolAtLast == '/' || checkingSymbolAtLast == '%' || checkingSymbolAtLast == '.') {
-        str = str.slice(0, -1)
-        checkingSymbolAtLast = str.charAt(str.length - 1);
+    // remove any trailing operators
+    switch (str.charAt(str.length - 1)) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '%':
+        case '.':
+            str = str.slice(0, -1);
+            break;
     }
-    // if there is only symbol in input field then this condition will run
-    if (currentData == '+' || currentData == '-' || currentData == '*' || currentData == '/' || currentData == '%' || currentData == '.') {
+
+    // if there is only operator in input field then set output to 0
+    if (/^[\+\-\*\/\%\.]$/.test(currentData)) {
         outputResult.value = 0;
     } else {
-        let setData = eval(str); //perform javascript operations on string values
-
-        //remove the digits from . after two digits
-        let text = setData.toString();
-        if (text.indexOf(".")) {
-
-            let indexOfDot = text.indexOf("."); // indexOf of first . decimal digit
-            let totalDigitAfterDot = text.length - indexOfDot - 1; // calculate total digits after decimal
-            let checkValueIndex = indexOfDot + 3; // check indexing for remove further digits
-            if (totalDigitAfterDot > 3) {
-                if (text[checkValueIndex] > 5) {
-                    // remove the digits by looping & loops run from 4th index
-                    for (let i = 4; i < text.length; i++) {
-                        if (text[i] < 5) {
-                            setData = text.slice(0, indexOfDot + i - 1)
-                            break;
-                        }
-                    }
-                } else {
-                    // removing the digits if 3rd digit after decimal is less than 5
-                    setData = text.slice(0, -Math.abs(totalDigitAfterDot - 2));
-                }
-            }
-        }
+        // evaluate the expression
+        let setData = Function('"use strict";return (' + str + ')')();
+        setData = parseFloat(setData.toFixed(2));
 
         outputResult.value = setData;
         result.value = setData;
     }
     resultStatus = 1;
 }
+
+// get notification according to keypress
+const keyToNotification = {
+    // 96: ["Zero", "None", "#", 0],
+    48: ["Zero", "None", "#", 0],
+    // 97: ["One", "None", "#", 1],
+    49: ["One", "None", "#", 1],
+    // 98: ["Two", "None", "#", 2],
+    50: ["Two", "None", "#", 2],
+    // 99: ["Three", "None", "#", 3],
+    51: ["Three", "None", "#", 3],
+    // 100: ["Four", "None", "#", 4],
+    52: ["Four", "None", "#", 4],
+    // 101: ["Five", "None", "#", 5],
+    53: ["Five", "None", "#", 5],
+    // 102: ["Six", "None", "#", 6],
+    54: ["Six", "None", "#", 6],
+    // 103: ["Seven", "None", "#", 7],
+    55: ["Seven", "None", "#", 7],
+    // 104: ["Eight", "None", "#", 8],
+    56: ["Eight", "None", "#", 8],
+    // 105: ["Nine", "None", "#", 9],
+    57: ["Nine", "None", "#", 9],
+
+    // 107: ["Plus", "None", "#", "+"],
+    // 109: ["Minus", "None", "#", "-"],
+    // 189: ["Minus", "None", "#", "-"],
+    // 106: ["Multiply", "None", "#", "*"],
+    // 111: ["Divide", "None", "#", "/"],
+
+    // 110: ["Dot", "None", "#", "."],
+    190: ["Dot", "None", "#", "."],
+
+    187: ["=", "warning", "#", "="],
+    // 13: ["=", "warning", "#", "="],
+
+    8: ["Backspace", "warning", "B", "backSpace"],
+
+    // also used for delete
+    // 46: ["Clear", "info", "X", "clear"]
+};
+
+document.addEventListener("keydown", function (e) {
+    console.log("key", e.key)
+    console.log("keyCode", e.keyCode)
+    if (calculatorStatus == 1) {
+        const notificationArgs = keyToNotification[e.keyCode];
+        if (notificationArgs) {
+            notification(...notificationArgs);
+        } else {
+            let isShift;
+            if (window.event) {
+                isShift = !!window.event.shiftKey; // typecast to boolean
+            } else {
+                isShift = !!ev.shiftKey;
+            }
+            if (isShift) {
+                switch (e.keyCode) {
+                    case 187:
+                        notification("Plus", "None", "#", "+");
+                        break;
+                    case 56:
+                        notification("Multiply", "None", "#", "*");
+                        break;
+                    case 53:
+                        notification("Percentage", "None", "#", "%");
+                        break;
+                    default:
+                        showNotification("Invalid Key Pressed", "red", "question");
+                        break;
+                }
+            } else {
+                showNotification("Invalid Key Pressed", "red", "question");
+            }
+        }
+    } else {
+        showNotification("Turn On your Calc", "red", "question");
+    }
+});
--- README.md
+++ README.md
@@ -1,17 +1,99 @@
 # Calculator
 
-## [Watch it on youtube](https://www.youtube.com/c/CodeWithPankaj1)
+In this webpage, you can perform any type of calculation using our web-based calculator, and we guarantee that you will be satisfied with the results.
 
-## [Live Preview of Project](https://pankajbaliyan.github.io/Calculator/)
+<hr>
 
-### The description of a Calculator Project
+💙 Join the channel to see more videos like this. [Code_With_Pankaj](https://www.youtube.com/c/CodeWithPankaj1?sub_confirmation=1)
+<hr>
 
-- Both the keyboard and buttons can be used to access the calculator..
-- Tracking previous results is easy.
-- The ability to perform any type of calculation.
-- Any keypress will trigger Snackbar Notifications.
-- Compatible with all mobile & desktop devices and with a beautiful and pleasant user interface.
+<div align=center>
 
-💙 Join the channel to see more videos like this. [Code_With_Pankaj](https://www.youtube.com/c/CodeWithPankaj1)
+## Must visit 👇 (Click on it)
 
-![preview img](/preview.png)
+[<img src="https://lh3.googleusercontent.com/3zkP2SYe7yYoKKe47bsNe44yTgb4Ukh__rBbwXwgkjNRe4PykGG409ozBxzxkrubV7zHKjfxq6y9ShogWtMBMPyB3jiNps91LoNH8A=s500" width="150" style="border-radius:10px">](https://www.youtube.com/c/CodeWithPankaj1?sub_confirmation=1)
+
+</div>
+
+<hr>
+
+## Features
+
+  - A responsive design.
+  - Effect of hovering.
+  - The width of the device will be automatically fixed.
+  - Calculators can be accessed with both the keyboard and buttons.
+  - Keeping track of previous results is easy.
+  - Any type of calculation can be performed.
+  - Snackbar Notifications will be triggered by any keyPress.
+  - With a beautiful and pleasant user interface, it is compatible with all mobile and desktop devices.
+
+
+## Authors
+
+- [@pankajbaliyan](https://www.github.com/pankajbaliyan)
+
+
+## Contributing
+
+Contributions are always welcome!
+
+See `index.html` for ways to get started.
+
+Please adhere to this project's `code of conduct`.
+
+
+## Demo
+
+https://calculator-cwd.vercel.app
+<br><br>
+https://pankajbaliyan.github.io/Calculator/
+
+
+## Feedback
+
+If you have any feedback, please reach out to us at pankajbaliyan90@gmail.com
+
+
+## 🔗 Links
+
+<div style="display:flex; justify-content: space-between">
+
+[<img src="https://www.edigitalagency.com.au/wp-content/uploads/linkedin-logo-gif-funny-man-suitcase.gif" width="150" style="background-color:white;padding:5px;border-radius:5px">](https://www.linkedin.com/in/pankaj-kumar-90/)
+
+[<img src="https://static.wixstatic.com/media/b83005_809e60f5dae943ddb60598d5d8343100~mv2.gif" width="150" style="background-color:white;padding:5px;border-radius:5px">](https://codewithpankaj.vercel.app)
+
+</div>
+
+## Lessons Learned
+
+I learned many things while making this repository, i.e. how capture keyboard events & get keyCode from that event and use it throughout the project, how to make notifications as well as notifications based on keyPressed.
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/PankajBaliyan/Calculator.git
+```
+
+Go to the project directory
+
+```bash
+  cd my-project
+```
+
+Start code editor
+
+```bash
+  code .
+```
+
+
+## Screenshots
+
+![App Screenshot](./preview.webp)
+
+
+## Support
+
+For support, email pankajbaliyan90@gmail.com or join our Slack channel.
